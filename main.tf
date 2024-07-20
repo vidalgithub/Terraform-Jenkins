1b76a823045475c74abce42f2895fa1aa7ee0ecc
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-07-20 05:46:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
-
-resource "aws_security_group" "TF-Jenkins-Instance" {
+resource "aws_security_group" "tf_jenkins_instance" {
   name        = "TF-Jenkins-Instance"
   description = "Allow inbound traffic to specific ports and outbound to all destinations"
 
@@ -53,32 +52,36 @@ resource "aws_security_group" "TF-Jenkins-Instance" {
 ######## PROVISION INFRASTRUCTURE  #################
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["099720109477"] # Canonical
+  owners      = ["099720109477"] # Canonical
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   filter {
     name   = "root-device-type"
     values = ["ebs"]
   }
+
   filter {
     name   = "architecture"
     values = ["x86_64"]
   }
 }
 
-# provisdion and EC2 instance
-resource "aws_instance" "TF-Jenkins-Instance" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+# Provision an EC2 instance
+resource "aws_instance" "tf_jenkins_instance" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
-  key_name      = "terraform-key"
-  vpc_security_group_ids = [aws_security_group.TF-Jenkins-Instance.id]
+  key_name                    = "terraform-key"
+  vpc_security_group_ids      = [aws_security_group.tf_jenkins_instance.id]
 
   tags = {
     Name = "TF-Jenkins-Instance"
@@ -86,5 +89,5 @@ resource "aws_instance" "TF-Jenkins-Instance" {
 }
 
 output "public_ip" {
-  value = aws_instance.TF-Jenkins-Instance.public_ip
+  value = aws_instance.tf_jenkins_instance.public_ip
 }
