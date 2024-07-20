0a2c7fbb96dc1ecf11f7baaf6ee38701386a9434
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-07-20 05:00:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,89 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
+
+resource "aws_security_group" "TF-Jenkins-Instance" {
+  name        = "TF-Jenkins-Instance"
+  description = "Allow inbound traffic to specific ports and outbound to all destinations"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # ingress {
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+######## PROVISION INFRASTRUCTURE  #################
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+# provisdion and EC2 instance
+resource "aws_instance" "TF-Jenkins-Instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  key_name      = "terraform-key"
+  vpc_security_group_ids = [aws_security_group.TF-Jenkins-Instance.id]
+
+  tags = {
+    Name = "TF-Jenkins-Instance"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.TF-Jenkins-Instance.public_ip
 }
